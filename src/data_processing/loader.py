f25390da43a04f89acefa7b48f3f003945a0e16b
Vinayak <kvinayak2579@gmail.com>
2026-03-05 12:07:54 +0530

    feat: Allow processing on PDFs

--- src/data_processing/loader.py
+++ src/data_processing/loader.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from langchain_community.document_loaders import (
     DirectoryLoader,
+    PyPDFLoader,
     TextLoader,
     UnstructuredMarkdownLoader,
 )
@@ -29,10 +30,19 @@ class DocumentLoader:
         )
         return loader.load()
 
+    def load_pdf_files(self):
+        loader = DirectoryLoader(
+            path=str(self.data_dir),
+            glob="**/*.pdf",
+            loader_cls=PyPDFLoader,
+        )
+        return loader.load()
+
     def load_all_documents(self):
         all_docs = []
         all_docs.extend(self.load_markdown_files())
         all_docs.extend(self.load_text_files())
+        all_docs.extend(self.load_pdf_files())
         return all_docs
 
 
